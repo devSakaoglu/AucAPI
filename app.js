bb44954e9701fef678db747ebe4ab27baa6b72a4
Muhamedtalha Sakaoglu <mtsakaoglu@gmail.com>
2024-06-19 23:27:41 +0300

    login-register

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-import express from "express";
-import jwt from "jsonwebtoken";
 import db from "./Db.js";
+import express from "express";
+import { password } from "bun";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,42 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+const Users = mongoose.model("Users", {
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  cardData: { type: Object },
+  date: { type: Date, default: Date.now },
+});
+
+app.post("/signup", async (req, res) => {
+  let check = await Users.findOne({ email: req.body.email });
+  if (check) {
+    return res
+      .status(400)
+      .json({ success: false, errors: "existing user found with same email" });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cardData: cart,
+  });
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
 });
 
 export default app;
