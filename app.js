d8c8cd9e30a514475c738b4681a18935e89f353d
Muhamedtalha Sakaoglu <mtsakaoglu@gmail.com>
2024-06-19 23:18:03 +0300

    Some Updates

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-import mongoose from 'mongoose';
-import db from './Db.js';
-import express from 'express';
-import { password } from 'bun';
+import mongoose from "mongoose";
+import express from "express";
+import jwt from "jsonwebtoken";
+import db from "./Db.js";
 
 const app = express();
+app.use(express.json());
 // Define your routes and middleware here
-app.get('/', (req, res) => {
-    const data = {
-        message: 'Hello World!'
-    };
-    res.json(data);
+app.get("/", (req, res) => {
+  const data = {
+    message: "Hello World!",
+  };
+  res.json(data);
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
-const Users=mongoose.model('Users',{
-name:{type:String},
-email:{type:String,
-    unique:true,},
-password:{type:String},
-cardData:{type:Object},
-date:{type:Date,default:Date.now}
-})
+const Users = mongoose.model("Users", {
+  name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  cardData: { type: Object },
+  date: { type: Date, default: Date.now },
+});
 
-app.post('/signup', async(req,res) => {
+app.post("/signup", async (req, res) => {
+  const model = await Users.findOne({ email: req.body.email });
+  if (model) {
+    return res.status(400).json({
+      data: model,
+      success: false,
+      errors: "existing user found with same email",
+    });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cardData: cart,
+  });
+  await user.save();
 
-    let check = await Users.findOne({email:req.body.email});
-    if(check){
-        return res.status(400).json({success:false,errors:"existing user found with same email"})
-    }
-    let cart={};
-    for (let i = 0; i < 300; i++) {
-        cart[i]=0;
-    }
-    const user= new Users({
-        name:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
-        cardData:cart,
-
-    })
-    await user.save();
-    
-    const data ={
-        user:{
-            id:user.id
-        }
-    }
-    const token=jwt.sign(data,'secret_ecom');
-    res.json({success:true,token})
-})
-
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
 
 export default app;
