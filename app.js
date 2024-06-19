6202bf321cb781f21b9e3b4e72521c2fed689ad1
Taner <tanerileri25@gmail.com>
2024-06-19 19:55:50 +0300

    dev

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+import mongoose from 'mongoose';
 import db from './Db.js';
 import express from 'express';
+import { password } from 'bun';
 
 const app = express();
 // Define your routes and middleware here
@@ -13,6 +15,43 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+const Users=mongoose.model('Users',{
+name:{type:String},
+email:{type:String,
+    unique:true,},
+password:{type:String},
+cardData:{type:Object},
+date:{type:Date,default:Date.now}
+})
+
+app.post('/signup', async(req,res) => {
+
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email"})
+    }
+    let cart={};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+    const user= new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cardData:cart,
+
+    })
+    await user.save();
+    
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const token=jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
 
 
 export default app;
