918c0c8d0e81d92bc407ca4e3d6d424db6f536a6
Muhammedtalha Sakaoglu <mtsakaoglu@gmail.com>
2024-06-24 21:38:32 +0300

    app.js clean

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// import mongoose from "mongoose";
-import AppUserRouter from "./api/AppUserRouter.js"; // Import AppUserRouter
-import BidRouter from "./api/BidRouteri.js"; // Import BidRouter
+import AppUserRouter from "./api/AppUserRouter.js";
+import BidRouter from "./api/BidRouteri.js";
 import ProductRouter from "./api/ProductRouter.js";
-import { AppUser, Bid, Product } from "./Db.js";
 import express from "express";
-import jwt from "jsonwebtoken";
 import cors from "cors";
 const PORT = 5000;
 const app = express();
@@ -31,40 +28,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 //
-app.post("/signup", async (req, res) => {
-  let check = await AppUser.findOne({ email: req.body.email });
-  if (check) {
-    return res
-      .status(400)
-      .json({ success: false, errors: "existing user found with same email" });
-  }
-
-  const user = new AppUser({
-    name: req.body.name,
-    surname: req.body.surname,
-    phone: req.body.phone,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  await AppUser.save();
-
-  const data = {
-    user: {
-      id: user.id,
-    },
-  };
-  const token = jwt.sign(data, "secret_ecom");
-  res.json({ success: true, token });
-});
-
-app.post("/login/default", async (req, res) => {
-  const user = await AppUser.find();
-  const userID = user[0].id;
-  console.log(userID);
-  const token = jwt.sign({ userID, role: "AppUser" }, "secret key");
-  const token_Data = jwt.verify(token, "secret key");
-  res.json({
-    token,
-    token_Data,
-  });
-});
